043090bc2d0f5aca2a3130f22753da2eeff6f968
guruabhay25 <guptaabhay2727@gmail.com>
2026-08-16 13:40:05 +0530

    server started and db connected successfully

---
+++ Backend/src/db/db.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+const mongoose = require('mongoose');
+
+async function connectDB(){
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Database connected successfully")
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+    }
+}
+
+module.exports = connectDB;
