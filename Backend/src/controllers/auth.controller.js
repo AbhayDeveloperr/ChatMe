d9177778f15da29c06531f514fcde921b9006b6c
guruabhay25 <guptaabhay2727@gmail.com>
2026-08-17 01:26:22 +0530

    loginUser api created

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -44,4 +44,47 @@ async function registerUser(req,res){
 }
 
 
-module.exports = {registerUser}
+async function loginUser(req,res) {
+    
+    const {email, username, password} = req.body
+
+    //is username ya email se koi user hai ya nahi
+    const user = await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+
+    if(!user){
+        return res.status(401).json({
+            message: "Invalid credentials"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if(!isPasswordValid){
+        return res.status(401).json({message: "invalid Credentials"})
+    }
+
+    const token = jwt.sign({
+        id: user._id,
+    },process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user:{
+            id: user._id,
+            username: user.username,
+            email: user.email,
+        }
+    })
+}
+
+
+
+
+module.exports = {registerUser, loginUser}
