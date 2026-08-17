7fdc3baf9108b98d815802eb1d10a74269207b9a
guruabhay25 <guptaabhay2727@gmail.com>
2026-08-17 23:12:44 +0530

    getMessages Api created

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -16,4 +16,20 @@ async function sendMessage(req, res) {
     });
 }
 
-module.exports = {sendMessage};
+async function getMessages(req, res) {
+
+    const { conversationId } = req.params;
+
+    const messages = await messageModel.find({
+        conversation: conversationId
+    }).sort({
+        createdAt: 1           //ye messages ko oldest se newest form me dikhayega
+    });
+
+    res.status(200).json({
+        messages
+    });
+}
+
+
+module.exports = {sendMessage, getMessages};
--- Backend/src/routes/message.route.js
+++ Backend/src/routes/message.route.js
@@ -5,5 +5,7 @@ const authMiddleware = require("../middlewares/auth.middleware");
 const router = express.Router();
 
 router.post("/", authMiddleware, messageController.sendMessage);
+router.get("/:conversationId", authMiddleware, messageController.getMessages);
+
 
 module.exports = router;
