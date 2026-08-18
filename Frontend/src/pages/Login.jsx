7638e7e3a16ce52881719783b9b2435c3ad5289b
guruabhay25 <guptaabhay2727@gmail.com>
2026-08-19 00:29:05 +0530

    login page implemented

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,12 +1,43 @@
+import axios from 'axios';
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+
+    const [loginInput, setLoginInput] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+        const response = await axios.post(
+            "http://localhost:3000/api/auth/login",
+            {
+                username: loginInput,
+                email: loginInput,
+                password: password
+            },
+            {
+                withCredentials: true
+            }
+        );
+        console.log(response.data);
+
+    } catch (error) {
+        console.log(error);
+        console.log(error.response?.data);
+    }
+};
+
+
+
   return (
     <div>
        <div className='h-screen bg-[#FFF7EB] flex justify-center items-center'>
             <div className='w-md bg-[#D0E7E6] border border-gray-100 p-8 rounded-md shadow'>
                 <h2 className="text-xl font-semibold text-black underline mb-4">Login your ChatMe Account</h2>
-                <form className='space-y-2'>
+                <form onSubmit={handleLogin} className='space-y-2'>
                     <div>
                         <label htmlFor="username" className="block text-sm font-medium text-gray-700">
                         Username or email
@@ -15,6 +46,8 @@ const Login = () => {
                         type="text"
                         id="email"
                         name="email"
+                        value={loginInput}
+                        onChange={(e)=> setLoginInput(e.target.value)}
                         required
                         className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
                         />
@@ -27,22 +60,19 @@ const Login = () => {
                         type="password"
                         id="password"
                         name="password"
+                        value={password}
+                        onChange={(e)=> setPassword(e.target.value)}
                         required
                         className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
                         />
                     </div>
                     
-                    {/* {message && (
-                        <p className="text-lg text-center text-green-600">
-                            {message}
-                        </p>
-                    )} */}
 
                     <button
                         type="submit"
                         className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-4 rounded cursor-pointer"
                     >
-                        Register
+                        Login
                     </button>
                     <div>
                         <p className="text-sm text-gray-600">
