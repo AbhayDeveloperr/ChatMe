bd277d35b41f4a2e9f8e8cf96c127948849af58a
guruabhay25 <guptaabhay2727@gmail.com>
2026-08-18 17:10:53 +0530

    Register and Login page created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      abhay
+      <Router>
+        <Routes>
+          <Route path='/register' element={<Register/>} />
+          <Route path='/login' element={<Login/>} />
+        </Routes>
+      </Router>
     </div>
   )
 }
--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,29 +1,18 @@
 import React from 'react'
 
-const Register = () => {
+const Login = () => {
   return (
-    <div className='h-screen bg-[#E3F2FD] flex justify-center items-center'>
-            <div className='w-md bg-gray-100 border border-gray-100 p-8 rounded-md shadow'>
-                <h2 className="text-xl font-semibold text-black underline mb-4">Register your ChatMe Account</h2>
+    <div>
+       <div className='h-screen bg-[#FFF7EB] flex justify-center items-center'>
+            <div className='w-md bg-[#D0E7E6] border border-gray-100 p-8 rounded-md shadow'>
+                <h2 className="text-xl font-semibold text-black underline mb-4">Login your ChatMe Account</h2>
                 <form className='space-y-2'>
                     <div>
                         <label htmlFor="username" className="block text-sm font-medium text-gray-700">
-                        Username
+                        Username or email
                         </label>
                         <input
                         type="text"
-                        id="username"
-                        name="username"
-                        required
-                        className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500"
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                        Email
-                        </label>
-                        <input
-                        type="email"
                         id="email"
                         name="email"
                         required
@@ -57,13 +46,14 @@ const Register = () => {
                     </button>
                     <div>
                         <p className="text-sm text-gray-600">
-                        Already have an account? <a href="/login" className="text-blue-500 hover:underline cursor-pointer">Login</a>
+                        Already have an account? <a href="/register" className="text-blue-500 hover:underline cursor-pointer">Register</a>
                         </p>
                     </div>
                 </form>
             </div>
+        </div>
     </div>
   )
 }
 
-export default Register
+export default Login
