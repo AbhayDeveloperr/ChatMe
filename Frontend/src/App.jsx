550a842871e873ddd9357e21f4fcda509a038e88
guruabhay25 <guptaabhay2727@gmail.com>
2026-08-19 00:50:43 +0530

    Sidebar and ChatWindow component created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Register from './pages/Register'
 import Login from './pages/Login'
 import {BrowserRouter as Router , Routes , Route} from 'react-router-dom'
+import Sidebar from './components/Sidebar/Sidebar'
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path='/register' element={<Register/>} />
           <Route path='/login' element={<Login/>} />
+          <Route path='/Chat' element={<Sidebar/>} />
         </Routes>
       </Router>
     </div>
