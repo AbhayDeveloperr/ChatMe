7e415f4f02596f285ab2fb1482f3cf1db66f2a5d
guruabhay25 <guptaabhay2727@gmail.com>
2026-08-20 00:03:59 +0530

    map function in chatList

--- Frontend/src/components/ChatWindow/ChatHeader.jsx
+++ Frontend/src/components/ChatWindow/ChatHeader.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const ChatHeader = () => {
   return (
-    <div className="h-16 px-5 bg-white border-b border-gray-200 flex items-center justify-center">
-      <h2 className="font-semibold text-gray-800">
+    <div className="h-16 px-8 bg-white border-b border-gray-200 flex items-center">
+      <h2 className="font-semibold text-xl text-gray-800">
         Guru
       </h2>
     </div>
--- Frontend/src/components/Sidebar/ChatList.jsx
+++ Frontend/src/components/Sidebar/ChatList.jsx
@@ -1,24 +1,30 @@
 import React from 'react'
 
 const ChatList = () => {
+
+  const chats = [
+    {
+      username: "Guru",
+      lastMessage: "Hey, how are you?"
+    },
+    {
+      username: "Ashish",
+      lastMessage: "What's up?"
+    },
+    {
+      username: "Shreya",
+      lastMessage: "Hello!"
+    }
+  ]
+
   return (
     <div>
-      
-      <div className="px-4 py-3 border-b border-gray-200">
-        <h3 className="font-semibold">Rahul</h3>
-        <p className="text-sm text-gray-500">Hey, how are you?</p>
-      </div>
-
-      <div className="px-4 py-3 border-b border-gray-200">
-        <h3 className="font-semibold">Aman</h3>
-        <p className="text-sm text-gray-500">What's up?</p>
-      </div>
-
-      <div className="px-4 py-3 border-b border-gray-200">
-        <h3 className="font-semibold">Priya</h3>
-        <p className="text-sm text-gray-500">Hello!</p>
-      </div>
-
+      {chats.map((chat,index)=> (
+        <div key={index} className="px-4 py-3 border-b border-gray-200">
+          <h3 className="font-semibold">{chat.username}</h3>
+          <p className="text-sm text-gray-500">{chat.lastMessage}</p>
+        </div>
+      ))}
     </div>
   )
 }
