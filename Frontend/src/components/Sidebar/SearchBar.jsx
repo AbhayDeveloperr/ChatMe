1f0f7e1b0af0aa8bd5f527993a491dfd9fdc3f54
guruabhay25 <guptaabhay2727@gmail.com>
2026-08-21 01:34:07 +0530

    SearchUsers api implemented

--- Frontend/src/components/Sidebar/SearchBar.jsx
+++ Frontend/src/components/Sidebar/SearchBar.jsx
@@ -6,9 +6,38 @@ const SearchBar = () => {
   const [search, setSearch] = useState("");
   const [users, setUsers] = useState([]);
 
+  const handleSearch = async (value) => {
+    try {
+      const response = await axiosInstance.get(
+        `/users/search?username=${value}`
+      );
+
+      console.log(response.data.users);
+
+      setUsers(response.data.users);
+
+    } catch (error) {
+      console.log(error.response?.data);
+    }
+  };
+
   return (
     <div className='p-4'>
-        <input type="text" placeholder='Search user..' value={search} onChange={(e) => setSearch(e.target.value)} className='w-full px-4 py-2 bg-gray-100 rounded-lg outline-none' />
+        <input type="text" placeholder='Search user..' value={search} onChange={(e) => {
+          setSearch(e.target.value)
+          handleSearch(e.target.value)
+        }} className='w-full px-4 py-2 bg-gray-100 rounded-lg outline-none' />
+
+        {users.length > 0 && (
+          <div className="mt-2 bg-white border border-gray-200 rounded-lg shadow">
+            {users.map((user) => (
+              <div key={user._id} className="px-4 py-3 border-b border-gray-100 hover:bg-gray-100 cursor-pointer">
+                <h3 className="font-medium">{user.username}</h3>
+                <p className="text-sm text-gray-500">{user.email}</p>
+              </div>
+            ))}
+          </div>
+        )}
     </div>
   )
 }
