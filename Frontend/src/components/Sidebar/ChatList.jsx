6ffcb3381a869a00a33578b613445b20fe2eeea4
guruabhay25 <guptaabhay2727@gmail.com>
2026-08-22 20:39:51 +0530

    error fixed

--- Frontend/src/components/Sidebar/ChatList.jsx
+++ Frontend/src/components/Sidebar/ChatList.jsx
@@ -1,19 +1,17 @@
 import React from 'react'
 
-const ChatList = ({setSelectedChat, conversations}) => {
-
-
+const ChatList = ({ setSelectedChat, conversations }) => {
   return (
-  <div>
-    {conversations.map((chat) => (
-      <div key={chat._id} onClick={() => setSelectedChat(chat)} className="px-4 py-3 border-b border-gray-200 cursor-pointer hover:bg-gray-100">
-        <h3 className="font-semibold">{chat.username}</h3>
-        <p className="text-sm text-gray-500">
-          Start chatting...
-        </p>
-      </div>
-    ))}
-  </div>
+    <div>
+      {conversations.map((chat) => (
+        <div key={chat.conversationId} onClick={() => setSelectedChat(chat.user)} className="px-4 py-3 border-b border-gray-200 cursor-pointer hover:bg-gray-100">
+          <h3 className="font-semibold">{chat.user.username}</h3>
+          <p className="text-sm text-gray-500">
+            Start chatting...
+          </p>
+        </div>
+      ))}
+    </div>
   )
 }
 
